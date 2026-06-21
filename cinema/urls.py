2d71474ffe506c87fd1f748b35183a44bae7335a
TomekSzatal <szatalowicztomek@gmail.com>
2026-06-21 13:27:48 +0200

    Bugs fixed

--- cinema/urls.py
+++ cinema/urls.py
@@ -17,7 +17,7 @@ router.register("movies", MovieViewSet)
 urlpatterns = [
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
-        path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path(
         "cinema_halls/",
